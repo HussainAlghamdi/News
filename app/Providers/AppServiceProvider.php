8cdbc0580d14c35ee57187a2c38c72b18357db25
Hussain <H@gmail.com>
2021-07-27 16:19:21 +0300

    deployment test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,8 +21,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+    
     public function boot()
     {
-        //
+        if(env('APP_ENV')!=='local'){
+            URL::forceScheme('https');
+        }
     }
 }
